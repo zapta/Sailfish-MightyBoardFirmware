8fb6f54d69d6a117504b1ba829db16edf25af2d5
Zapta <zapta@zapta.com>
2016-05-12 19:05:08 -0700

    Added a comment.

--- pull_fork.sh
+++ pull_fork.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+# Based on instructions here:
+# https://help.github.com/articles/merging-an-upstream-repository-into-your-fork/
+
 git checkout master
 
 git pull https://github.com/jetty840/Sailfish-MightyBoardFirmware.git
